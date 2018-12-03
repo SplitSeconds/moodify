806dae4575ef68420076008d54890c8b99470332
Lindsay Wilson <lindsaywilsonlcw@gmail.com>
2018-12-03 21:30:19 +0100

    Fix Animation.js bracket

--- client/src/components/pages/Animation.js
+++ client/src/components/pages/Animation.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react'
-import Lottie from 'lottie-react-web'
-import bodymovin from 'bodymovin'
-import animation from '../../animation/Moodify_Logo.json'
+import React, { Component } from "react";
+import Lottie from "lottie-react-web";
+import bodymovin from "bodymovin";
+import animation from "../../animation/Moodify_Logo.json";
 
-
-
-  export default class Animation extends Component {
-    render() {
-      // var animation = bodymovin.loadAnimation({
-      //   container: document.getElementById('lottie'), // Required
-      //   path: 'data.json', // Required
-      //   renderer: 'svg', // Required
-      //   loop: true, // Optional
-      //   autoplay: true, // Optional
-      //   name: "Hello World", // Name for future reference. Optional.
-      return (  
-        <div className="Animation">
-        
-
-       </div>
-        
-      )
-    }
+export default class Animation extends Component {
+  render() {
+    // var animation = bodymovin.loadAnimation({
+    //   container: document.getElementById('lottie'), // Required
+    //   path: 'data.json', // Required
+    //   renderer: 'svg', // Required
+    //   loop: true, // Optional
+    //   autoplay: true, // Optional
+    //   name: "Hello World", // Name for future reference. Optional.
+    return <div className="Animation" />;
   }
 }
